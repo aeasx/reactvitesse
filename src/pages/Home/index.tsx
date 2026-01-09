955b7ef3eb1b254c2503f58ad294470394b2b080
肖振洋 <xiaozhenyang@huomakeji.com>
2026-01-09 15:04:04 +0800

    feat: 删除了一些内容

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,8 @@ import type { FC } from 'react'
 export const Home: FC = () => {
   return (
     <div className="w-[300px] h-[200px] m-auto text-3xl flex flex-col items-center">
-      <h1 className="text-red-600">react-ts-template</h1>
       <Icon icon="mdi:home" width="48" height="48" color="red" />
+      <p>Home</p>
     </div>
   )
 }
