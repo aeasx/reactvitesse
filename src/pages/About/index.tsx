3357454739c25a6bb74e8aed54a53785485b2c07
aeasx <3089326386@qq.com>
2026-02-03 21:32:33 +0800

    feat:checkoutbox demo

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,71 +1,61 @@
-import type { FC } from 'react'
-import { Icon } from '@iconify/react'
-import { Button, Form, Input, Space } from 'antd'
-
-const onFinish = (values: unknown) => {
-  console.log('Received values of form:', values)
-}
-
+import { Button, Checkbox, Col, ConfigProvider, Form, Row, theme, type CheckboxProps } from 'antd'
+import { useState, type FC } from 'react'
 export const About: FC = () => {
   const [form] = Form.useForm()
   return (
     <div>
       <h1 className="font-bold text-stone-600">About Page</h1>
-      <Form
-        form={form}
-        name="dynamic_form_nest_item"
-        onFinish={onFinish}
-        style={{ maxWidth: 600 }}
-        autoComplete="off"
-      >
-        <Form.List name="users">
-          {(fields, { add, remove }) => (
-            <>
-              {fields.map(({ key, name, ...restField }) => (
-                <Space
-                  key={key}
-                  style={{ display: 'flex', marginBottom: 8 }}
-                  align="baseline"
-                >
-                  <Form.Item
-                    {...restField}
-                    name={[name, 'first']}
-                    rules={[{ required: true, message: 'Missing first name' }]}
-                  >
-                    <Input placeholder="First Name" />
-                  </Form.Item>
-                  <Form.Item
-                    {...restField}
-                    name={[name, 'last']}
-                    rules={[{ required: true, message: 'Missing last name' }]}
-                  >
-                    <Input placeholder="Last Name" />
-                  </Form.Item>
-                  <Icon
-                    icon="subway:subtraction"
-                    onClick={() => remove(name)}
-                  />
-                </Space>
-              ))}
-              <Form.Item>
-                <Button
-                  type="dashed"
-                  onClick={() => add()}
-                  block
-                  icon={<Icon icon="ant-design:plus-outlined" />}
-                >
-                  Add field
-                </Button>
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
+      <Form form={form}>
+        <CheckBoxAll />
+        <Button type="primary" htmlType="submit">提交</Button>
       </Form>
     </div>
+  )
+}
+
+const checkedOptionsList = [
+  { label: '正信鸡排', value: '1' },
+  { label: '速走', value: '2' },
+  { label: '黄山', value: '3' },
+]
+
+const CheckBoxAll = () => {
+  const [checkedList, setCheckedList] = useState<string[]>(['1', '2']);
+  const checkAll = checkedOptionsList.length === checkedList.length;
+  const indeterminate = checkedList.length > 0 && checkedList.length < checkedOptionsList.length;
+  const onChange = (list: string[]) => {
+    setCheckedList(list);
+  };
+
+  const onCheckAllChange: CheckboxProps['onChange'] = (e) => {
+    setCheckedList(e.target.checked ? checkedOptionsList.map(item => item.value) : []);
+  };
+
+  return (
+    <ConfigProvider
+      theme={{ algorithm: theme.darkAlgorithm }}>
+      <Row>
+        <Col span={24}>
+          <Form.Item label='品牌' labelCol={{ span: 2 }}>
+            <Checkbox indeterminate={indeterminate} checked={checkAll} onChange={onCheckAllChange}>全选</Checkbox>
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} offset={2}>
+          <Form.Item>
+            <Checkbox.Group onChange={onChange} value={checkedList} >
+              {
+                checkedOptionsList.map(item => {
+                  return (
+                    <Checkbox value={item.value} key={item.value}>{item.label}</Checkbox>
+                  )
+                })
+              }
+            </Checkbox.Group>
+          </Form.Item >
+        </Col>
+      </Row>
+    </ConfigProvider >
   )
 }
